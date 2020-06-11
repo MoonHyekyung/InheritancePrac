b5f6420c49666b6d3500cc59ebf4c9cc9e0be61e
MoonHyekyung <moonsoft02@gmail.com>
2020-06-11 14:45:54 +0900

    초기 커밋.

---
+++ InheritancePrac/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> cats = new List<Cat>();
+            cats.Add(new Cat());
+            cats.Add(new Cat());
+            cats.Add(new Cat());
+
+            foreach(var item in dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+        }
+    }
+}
